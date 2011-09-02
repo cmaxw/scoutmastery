f9b74ce08b6e5f298f1bb5bf95f5f1c20f2342d6
Charles Max Wood <chuck@teachmetocode.com>
2011-09-02 09:42:35 -0600

    rails defaults

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scoutmastery::Application.config.secret_token = 'd9b04ba6792b6656153f3e35376921b883ff9635c6336031893e54b2bf5ce74b32bca3a35b744a648c75c68c87bdc5349430cd4c38690aeb926e4f5b4ab8631b'
